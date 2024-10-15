11b672a6c11893096ede13285bea1aab4e66e6d9
kael-cadranel <kael.cadranel00@gmail.com>
2024-10-15 15:10:57 +0200

    add mouse class

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,6 +1,7 @@
 import {BurgerMenu} from "./classes/BurgerMenu";
 import {createMarquees} from "./functions/createMarquees";
 import {createAccordions} from "./functions/createAccordions";
+import {Mouse} from "./classes/Mouse";
 
 document.addEventListener('DOMContentLoaded', function() {
     const noJs = document.querySelectorAll('.no-js');
@@ -8,4 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
     new BurgerMenu(document.querySelector('#mainMenu'), document.querySelector('#mainMenuButton'));
     createMarquees();
     createAccordions();
+    new Mouse(document.querySelector('#canvas'), {
+    });
 });
