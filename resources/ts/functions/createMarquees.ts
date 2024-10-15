8603c6094c82e292ada4ebf25fcc15300f46b868
kael-cadranel <kael.cadranel00@gmail.com>
2024-10-15 15:31:11 +0200

    Edit MarqueeConfig

--- resources/ts/functions/createMarquees.ts
+++ resources/ts/functions/createMarquees.ts
@@ -5,9 +5,10 @@ export function createMarquees() {
         if (e.children.length > 1) {
             return console.error('Your marquee container can only have one child');
         }
-        new Marquee({
-            container: e,
-            element: e.children[0] as HTMLElement,
-        })
+        const marquee = new Marquee(e, e.children[0] as HTMLElement, {})
+
+        if (e.dataset.direction) {
+            marquee.direction = e.dataset.direction;
+        }
     })
 }
