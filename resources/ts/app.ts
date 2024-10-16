efb81b6e7cd47b6b767245d197181ef533b668ac
kael-cadranel <kael.cadranel00@gmail.com>
2024-10-16 14:01:46 +0200

    Add js

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -3,6 +3,9 @@ import {createMarquees} from "./functions/createMarquees";
 import {createAccordions} from "./functions/createAccordions";
 import {Skew} from "./classes/Skew";
 import {Mouse} from "./classes/Mouse";
+import {observeElements} from "./functions/observeElements";
+import {Parallax} from "./classes/Paralax";
+import {PageAnimation} from "./classes/PageAnimation";
 
 document.addEventListener('DOMContentLoaded', function() {
     const noJs = document.querySelectorAll('.no-js');
@@ -12,8 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
     createAccordions();
     document.querySelectorAll('[data-skew]').forEach((e) => {
         new Skew(e as HTMLElement, {});
-    })
-    if(window.matchMedia(("(hover: none)")).matches) {
+    });
+    if(window.matchMedia(("(hover: hover)")).matches) {
         new Mouse(document.querySelector('#canvas'), {});
     }
+    observeElements();
+    document.querySelectorAll('[data-parallax]').forEach((e) => {
+        const element = e as HTMLElement;
+        new Parallax(element, {speed: parseInt(element.dataset.parallax) });
+    });
 });
