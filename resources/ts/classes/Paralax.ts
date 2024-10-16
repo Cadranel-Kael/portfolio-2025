649c047a1038b4335734c6f4ba50d982a941f4c0
kael-cadranel <kael.cadranel00@gmail.com>
2024-10-16 14:03:33 +0200

    Add parallax

---
+++ resources/ts/classes/Paralax.ts
@@ -0,0 +1,26 @@
+const DEFAULT_SPEED = 0.2;
+
+interface ParallaxConfig {
+    speed?: number;
+}
+
+export class Parallax {
+    private element: HTMLElement;
+    private scrollPosition: number;
+    private readonly speed: number;
+
+    constructor(element: HTMLElement, config: ParallaxConfig) {
+        this.element = element;
+        this.speed = config.speed || DEFAULT_SPEED;
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        window.addEventListener('scroll', () => {
+            this.scrollPosition = window.scrollY;
+
+            this.element.style.transform = `translateY(${this.scrollPosition * this.speed}px)`
+        })
+
+    }
+}
